2959ddcc53e16ca4a179a117f80c22b801c4379f
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-03-06 23:18:22 +0530

    navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,40 @@
+import { GoSearch } from "react-icons/go";
+import { MdAccountCircle } from "react-icons/md";
+import { AiOutlineCaretDown } from "react-icons/ai";
+import Image from "next/image";
+import thumbnail from "../public/images/thumbnail.svg";
+import Link from "next/link";
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="flex justify-center items-center w-full bg-[#0f171e] text-white">
+        <div className="flex items-center w-1/2 gap-3 p-4 mx-6">
+          <ul className="mt-4">
+            <Image src={thumbnail} width="120" alt="image" />
+          </ul>
+          <ul>Home</ul>
+          <ul>Store</ul>
+          <ul>Channels</ul>
+          <ul>Categories</ul>
+          <ul>
+            <AiOutlineCaretDown />
+          </ul>
+          <ul>My Stuff</ul>
+        </div>
+        <div className="flex items-center justify-end w-1/2 gap-3 p-4   mx-6">
+          <ol>
+            <GoSearch />
+          </ol>
+          <ol className="h-10">
+            <MdAccountCircle className="h-full w-full" />
+          </ol>
+          <ol>Harsh</ol>
+          <ol>
+            <AiOutlineCaretDown />
+          </ol>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
